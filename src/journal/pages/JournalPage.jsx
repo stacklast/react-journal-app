472c2b09ba624b42091d8e8acc287fbcf7666275
Edwin Benalcázar <Edwin.Benalcazar@rocketrez.com>
2022-12-21 11:42:10 -0500

    add: Config Initial Theme

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,12 @@
+import { MailOutline } from '@mui/icons-material'
+import { Typography } from '@mui/material'
 import React from 'react'
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <>
+    <Typography variant='h1'>JournalPage</Typography>
+    <MailOutline />
+    </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <JournalApp />
+      <JournalApp />
     </BrowserRouter>
   </React.StrictMode>,
 )
