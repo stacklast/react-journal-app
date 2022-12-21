97c5a5042e8a47bb093eaa92b3aa8be421d59c51
Edwin Benalcázar <Edwin.Benalcazar@rocketrez.com>
2022-12-21 14:24:17 -0500

    add: Sidebar Added

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,48 @@
+import { TurnedInNot } from "@mui/icons-material"
+import { Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+import { Box } from "@mui/system"
+
+export const SideBar = ({drawerWidth = 240}) => {
+  return (
+    <Box
+        component='nav'
+        sx={{width:{sm: drawerWidth}, flexShrink: {sm:0}}}
+    >
+        <Drawer
+            variant="permanent" // temporary
+            open
+            sx={{
+                display: {xs: 'block'},
+                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+            }}
+        >
+            <Toolbar>
+                <Typography variant="h6" noWrap component='div'>
+                    Edwin Benalcazar
+                </Typography>
+            </Toolbar>
+
+            <Divider />
+            <List>
+                {
+                    ['Enero', 'Febrero', 'Marzo', 'Abril'].map(text=>(
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <TurnedInNot />
+                                </ListItemIcon>
+                                <Grid container>
+                                    <ListItemText primary={text} />
+
+                                    <ListItemText  secondary={'Excercitation cillum irure elit consectretur.'}/>
+                                </Grid>
+                            </ListItemButton>
+                        </ListItem>
+                    ))
+                }
+            </List>
+
+        </Drawer>
+    </Box>
+  )
+}
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,15 +1,21 @@
+import { Toolbar } from "@mui/material";
 import { Box } from "@mui/system"
-import { NavBar } from "../components";
+import { NavBar, SideBar } from "../components";
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 export const JournalLayout = ({children}) => {
   return (
     <Box sx={{display:'flex'}}>
-        <NavBar />
+        <NavBar drawerWidth={drawerWidth} />
+
+        <SideBar drawerWidth={drawerWidth} />
         <Box component='main'
             sx={{flexGrowl: 1, p:3}}
         >
+          <Toolbar>
+            
+          </Toolbar>
             {children}
         </Box>
     </Box>
