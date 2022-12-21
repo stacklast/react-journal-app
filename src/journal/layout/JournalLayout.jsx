b02a675d6ee3dc89989bc2c4d0846ea7aac39f0f
Edwin Benalcázar <Edwin.Benalcazar@rocketrez.com>
2022-12-21 15:12:50 -0500

    add: Button

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -11,7 +11,7 @@ export const JournalLayout = ({children}) => {
 
         <SideBar drawerWidth={drawerWidth} />
         <Box component='main'
-            sx={{flexGrowl: 1, p:3}}
+            sx={{flexGrow: 1, p:3}}
         >
           <Toolbar />
           {children}
